341532c09ae82977e73482d1ac2bb107e4e17f9c
Tobias Nielsen <tn116@hotmail.com>
2020-08-27 00:28:40 +0200

    styling adjustments and menu functionality

--- src/Nav.js
+++ src/Nav.js
@@ -21,11 +21,11 @@ function Nav() {
         <div className={`nav ${show && "nav__black"}`}>
             <a className="nav__logo" href="http://localhost:3000/"><img src="https://upload.wikimedia.org/wikipedia/commons/archive/0/08/20190203185210%21Netflix_2015_logo.svg" alt="Netflix logo"/></a>
             <ul className="nav__menu">
-                <li>Home</li>
-                <li>Tv Shows</li>
-                <li>Movies</li>
-                <li>Latest</li>
-                <li>My List</li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Tv Shows</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Latest</a></li>
+                <li><a href="#">My List</a></li>
             </ul>
             <img className="nav__avatar" src="https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png" alt="avatar"/>
 
--- src/Row.js
+++ src/Row.js
@@ -23,10 +23,11 @@ function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
     const [trailerUrl, setTrailerUrl] = useState("")
 
+    console.log(movies);
 
-    // A snippet of code which runs based on a specific conditions/variables
+    //  runs based on a specific conditions/variables
     useEffect(()=> {
-        // if [], empty, run once when the row loads, and don't run again
+        // if [] empty, run once when the row loads, and don't run again
         async function fetchData(){
             const request = await axios.get(fetchUrl);    // wait for answer then do something
             // console.log(request.data.results);
@@ -39,10 +40,12 @@ function Row({title, fetchUrl, isLargeRow}) {
     }, [fetchUrl]);
 
 const handleClick = (movie) => {
+
     if (trailerUrl) {
         setTrailerUrl('');
     } else {
-        movieTrailer(movie?.name || "")
+        movieTrailer(movie?.name || movie?.title || "")
+        
         .then((url) => {
             const urlParams = new URLSearchParams(new URL(url).search);
             setTrailerUrl(urlParams.get('v')); // Gets vid ID value from url
@@ -60,7 +63,9 @@ const handleClick = (movie) => {
                     <img key={movie.id}
                     onClick={() => handleClick(movie)}
                     className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} loading="lazy"/>
+                    
                 ))}
+                
             </div>
                 {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} /> }
             {/*  container > posters/images */}
