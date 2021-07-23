2c464356f98e57f70345901b60d171f84bae38bd
Tobias Faucette <tn116@hotmail.com>
2021-07-23 15:41:44 +0200

    login form and styling

--- src/Login.js
+++ src/Login.js
@@ -18,7 +18,33 @@ function Login() {
           "Pay attention: this is not the original Netflix sign in. Don't insert
           your real credentials in this form"
         </small>
-        <form></form>
+        <form className="login__form">
+          <div className="login__form_inputWrap">
+            <input
+              type="text"
+              name="email"
+              placeholder="E-mail"
+              className="login__form__input"
+            ></input>
+            <label className="login__form__inputLabel">
+              Please Enter a valid email address
+            </label>
+          </div>
+          <div className="login__form_inputWrap">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="login__form__input"
+            ></input>
+            <label className="login__form__inputLabel">
+              The password should have a lenght between 6 and 30 charaters
+            </label>
+          </div>
+          <button type="submit" className="login__form__button submit__button">
+            Sign in
+          </button>
+        </form>
         <small className="login__content__register">
           Do you not have an account?{" "}
           <span className="login__content__toggle">Sign Up</span>
